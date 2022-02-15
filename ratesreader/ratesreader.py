0878585c023fac0dc02d0047731c3484bc69a261
akm77 <aleksey.kotryakhov@gmail.com>
2022-02-15 11:25:21 +1000

    Initial commit

--- ratesreader/ratesreader.py
+++ ratesreader/ratesreader.py
@@ -18,7 +18,7 @@ logging.basicConfig(
     level=logging.INFO,
     format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
 )
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
 @dataclass
 class DEFAULTS:
